4670205069c28a93f8f664faed08c8d02fdb6cf4
j25519 <212225829+j25519@users.noreply.github.com>
2025-05-19 19:01:38 +0100

    got rid of CSP Guard and just set the CSP manually for now

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import csp from "vite-plugin-csp-guard";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    csp({
-      // Enable outlierSupport for Tailwind CSS
-      outlierSupport: {
-        tailwind: true, // Ensures Tailwind's dynamic styles are CSP-compliant
-      },
-      // Define CSP policies
-      policies: {
-        'default-src': ["'self'"],
-        'img-src': ["'self'", "data:"],
-        'script-src-elem': ["'self'"],
-        'style-src-elem': ["'self'"],
-      },
-      // Apply CSP in both development and production
-      dev: {
-        enabled: true, // Enforce CSP in dev mode
-      },
-      build: {
-        enabled: true, // Enforce CSP in production builds
-        meta: true, // Inject CSP as meta tag in index.html
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     overlay: false, // Disable error overlay in dev mode
   },
